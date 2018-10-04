f047e253bd5a9b1e6ea7c2ccfe2a39693a1708c6
Tod Gentille <tod-gentille@Tods-Gray-Ghost.lan>
2018-10-04 15:10:03 -0700

    initial commit

---
+++ ImportData.js
@@ -0,0 +1,78 @@
+var ImportData = (function () {
+  'use strict';
+  const SRC_AUG_CATALOG_ID = "1U5Pv_Bljnl1hCn9yJetn8adnXmzi8JQe_RAPdoLcxOY";
+  const SRC_CATALOG_SHEET_NAME = "CourseData";
+  const DEST_CATALOG_SHEET_NAME = "SnapshotCourses";
+
+  const SRC_PIPELINE_ID = "1O1dP-KrenEVw5VFy6zI5xUbLXkj9S_4byy1ZNjvkaNA";
+  const SRC_PIPELINE_SHEET_NAME = "Sheet1";
+  const DEST_PIPELINE_SHEET_NAME = "PipelineCourses";
+
+
+  function getAugmentedCatalogSheet() {
+    const ss = SpreadsheetApp.openById(SRC_AUG_CATALOG_ID);
+    return ss.getSheetByName(SRC_CATALOG_SHEET_NAME);
+  }
+
+  function getData(sheet) {
+    const fullDataRange = sheet.getDataRange();
+    return fullDataRange.getValues();
+  }
+  function getAugmentedCatalogData() {
+    const sheet = getAugmentedCatalogSheet();
+    return getData(sheet);
+  }
+
+  function getPipelineSheet() {
+    const ss = SpreadsheetApp.openById(SRC_PIPELINE_ID)
+    return ss.getSheetByName(SRC_PIPELINE_SHEET_NAME);
+  }
+
+  function getPipelineData() {
+    const sheet = getPipelineSheet();
+    return getData(sheet);
+  }
+
+  function getSnapshotCoursesSheet() {
+    const ss = SpreadsheetApp.getActiveSpreadsheet();
+    const sheet = ss.getSheetByName(DEST_CATALOG_SHEET_NAME);
+    return sheet;
+  }
+
+  function getPipelineCoursesSheet() {
+    const ss = SpreadsheetApp.getActiveSpreadsheet();
+    return ss.getSheetByName(DEST_PIPELINE_SHEET_NAME);
+  }
+
+  function copyDataToSheet(data, sheet) {
+    const rows = data.length;
+    const cols = data[0].length;
+    sheet.getRange(1, 1, rows, cols).setValues(data);
+  }
+
+  function importAugmentedData() {
+    const fullData = getAugmentedCatalogData();
+    const sheet = getSnapshotCoursesSheet();
+    copyDataToSheet(fullData, sheet);
+  }
+
+  function importPipelineData() {
+    const fullData = getPipelineData();
+    const dest = getPipelineCoursesSheet();
+    copyDataToSheet(fullData, dest);
+
+  }
+
+  return {
+    importAugmentedData: importAugmentedData,
+    importPipelineData: importPipelineData
+  };
+
+}());
+
+/* exported importData */
+function importData() {
+  ImportData.importAugmentedData();
+  ImportData.importPipelineData();
+}
+
---
+++ ReadMe.md
@@ -0,0 +1,11 @@
+# Existing Course Search
+
+When curriculum needs to check for overlap we have to search both the current library and the courses in the pipeline. The code here along with the google sheet is designed to give us a way to search both at the same time and always be hitting current data. It will rely on the augmented nightly snapshot for the course data and it will rely on Hampton's python script that queries Salesforce for courses with stages of:
+Course Commissioned
+Course On Hold
+1st Module Approved
+75% of Modules Approved
+Course Delivered
+Topic Approved
+Topic Review 
+
